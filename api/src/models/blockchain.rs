91a8562d65a5d6ba1daeab260d2c6131140c8b3d
Nishant <69218488+NishantCoder108@users.noreply.github.com>
2025-06-30 22:08:42 +0530

    Create model for create token

--- api/src/models/blockchain.rs
+++ api/src/models/blockchain.rs
@@ -8,7 +8,34 @@ pub struct ApiResponse<T> {
 }
 
 #[derive(Serialize, Deserialize)]
+pub struct ApiSuccessResponse<T> {
+    pub success: bool,
+    pub data: Option<T>,
+}
+#[derive(Serialize, Deserialize)]
 pub struct KeypairResponse {
     pub pubkey: String,
     pub secret: String,
+}
+
+// For token Creation
+#[derive(Serialize, Deserialize)]
+pub struct CreateTokenRequest {
+    pub mint_authority: String,
+    pub mint: String,
+    pub decimals: u8,
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct InstructionResponse {
+    pub program_id: String,
+    pub accounts: Vec<AccountMetaResponse>,
+    pub instruction_data: String,
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct AccountMetaResponse {
+    pub pubkey: String,
+    pub is_signer: bool,
+    pub is_writable: bool,
 }
